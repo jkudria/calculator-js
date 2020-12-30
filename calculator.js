4b0cc180a0e63a07aca2e8f602b0b822e1e7728d
jkudria <jacob@kudria.net>
2020-12-30 03:23:16 -0500

    can now populate text area and fixed operation icons

--- calculator.js
+++ calculator.js
@@ -1,1 +1,5 @@
 const buttons = document.querySelectorAll('.button');
+const textArea = document.querySelector('#text-area');
+buttons.forEach(button => button.addEventListener('click', () => {
+	textArea.textContent += button.textContent;
+}));
