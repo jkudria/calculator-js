faecdf7654082683151ee6c826e26493106c7932
jkudria <jacob@kudria.net>
2020-12-31 03:03:08 -0500

    started basic input functionality

--- README.md
+++ README.md
@@ -4,5 +4,9 @@ Based on [lesson](https://www.theodinproject.com/courses/foundations/lessons/cal
 
 ## TODO
 
-[x] buttons scale down/up when clicked (i.e. pressed effect)
-* https://www.geeksforgeeks.org/how-to-add-a-pressed-effect-on-button-click-in-css/
+* [x] buttons scale down/up when clicked (i.e. pressed effect)
+	* https://www.geeksforgeeks.org/how-to-add-a-pressed-effect-on-button-click-in-css/
+* [ ] when operator pressed, start new number
+* [ ] if too many characters in text area, shrink them to fit
+* [ ] truncate decimal answers to X digits of accuracy
+* [ ] better to transmit buttons -> js var -> text area, or buttons -> text area -> js var?
--- calculator.js
+++ calculator.js
@@ -1,5 +1,62 @@
-const buttons = document.querySelectorAll('.button');
+const digitButtons = document.querySelectorAll('.digit');
+const operatorButtons = document.querySelectorAll('.operator');
 const textArea = document.querySelector('#text-area');
-buttons.forEach(button => button.addEventListener('click', () => {
-	textArea.textContent += button.textContent;
+const clearButton = document.querySelector('#clear');
+
+const operators = {
+	plus: (a, b) => a + b,
+	minus: (a, b) => a - b,
+	multiply: (a, b) => a * b,
+	divide: (a, b) => a / b,
+};
+
+const operatorStrings = ['plus', 'minus', 'multiply', 'divide'];
+console.log(typeof operatorStrings);
+
+let tokens = [];
+
+textArea.textContent = '0';
+
+clearButton.addEventListener('click', () => {
+	tokens = [];
+	textArea.textContent = '0';
+})
+
+digitButtons.forEach(digitButton => digitButton.addEventListener('click', () => {
+	// if currently just 0, we want to replace it with whatever user is inputting:
+	console.log(tokens);
+	console.log(isNaN(tokens[tokens.length - 1]));
+	if (textArea.textContent === '0') {
+		textArea.textContent = digitButton.textContent;
+	} else if (operatorStrings.includes(tokens[tokens.length - 1])) {
+		// if we just pressed an operator, we start a new number:
+		textArea.textContent = digitButton.textContent;
+	} else {
+		textArea.textContent += digitButton.textContent;
+	}
+
+	// console.log(textArea.textContent);
 }));
+
+operatorButtons.forEach(operatorButton => operatorButton.addEventListener('click', () => {
+	tokens.push(textArea.textContent);
+	tokens.push(operatorButton.id);
+	console.log(tokens);
+}));
+
+function processToken(button) {
+	if (button.classList.contains('operator')) {
+
+		tokens.push(button.id);
+	} else {
+		tokens.push(button.textContent);
+	}
+}
+
+function operate(a, b, operator) {
+	return operators[operator](a, b);
+}
+
+// console.log(operate(5, 6, 'divide'));
+
+5
